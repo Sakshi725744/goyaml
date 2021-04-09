41d1fafcac73c467b158d6888a99463c6d573f41
Sakshi725744 <sjparikh78@gmail.com>
2021-04-09 19:54:55 +0530

    adding output to examples

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,7 +11,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-func readConf(filename string) (*model.SpdxYaml, error) {
+func readConfYaml(filename string) (*model.SpdxYaml, error) {
 	buf, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -25,12 +26,20 @@ func readConf(filename string) (*model.SpdxYaml, error) {
 	return c, nil
 }
 
+
 func main() {
-	c, err := readConf("/home/sakshi7parikh/SPDX-YAML/examples/spdxYaml.yaml")
+	c, err := readConfYaml("./examples/spdxYaml.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
+	f, err := os.OpenFile("./examples/yamlOutput.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
 
+	if _, err := f.Write([]byte("Annotations2v2:\n\n")); err != nil {
+		log.Fatal(err)
+	}
 	annotations2v2 := parse.ParseAnnotation2v2(c)
 
 	for i := 0; i < len(annotations2v2); i++ {
@@ -39,14 +48,51 @@ func main() {
 		fmt.Println((annotations2v2)[i].AnnotatorType)
 		fmt.Println((annotations2v2)[i].Annotator)
 		fmt.Println((annotations2v2)[i].AnnotationDate)
+
+        //Writing annotations2v2 to ./examples/yamlOutput.txt
+		if _, err := f.Write([]byte((annotations2v2)[i].AnnotationComment+"\n")); err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := f.Write([]byte((annotations2v2)[i].AnnotationType+"\n")); err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := f.Write([]byte((annotations2v2)[i].AnnotatorType+"\n")); err != nil {
+			log.Fatal(err)
+		}
+
+		if _, err := f.Write([]byte((annotations2v2)[i].AnnotationDate+"\n\n")); err != nil {
+			log.Fatal(err)
+		}
 	}
 	annotations2v1 := parse.ParseAnnotation2v1(c)
-
+	if _, err := f.Write([]byte("Annotations2v1:\n\n")); err != nil {
+		log.Fatal(err)
+	}
 	for i := 0; i < len(annotations2v1); i++ {
 		fmt.Println((annotations2v1)[i].AnnotationComment)
 		fmt.Println((annotations2v1)[i].AnnotationType)
 		fmt.Println((annotations2v1)[i].AnnotatorType)
 		fmt.Println((annotations2v1)[i].Annotator)
 		fmt.Println((annotations2v1)[i].AnnotationDate)
+
+		//Writing annotations2v1 to ./examples/yamlOutput.txt
+		if _, err := f.Write([]byte((annotations2v1)[i].AnnotationComment+"\n")); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := f.Write([]byte((annotations2v1)[i].AnnotationType+"\n")); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := f.Write([]byte((annotations2v1)[i].AnnotatorType+"\n")); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := f.Write([]byte((annotations2v1)[i].AnnotationDate+"\n\n")); err != nil {
+			log.Fatal(err)
+		}
 	}
+	   
+    if err := f.Close(); err != nil {
+        log.Fatal(err)
+    }
 }
